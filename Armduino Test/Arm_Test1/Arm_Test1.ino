a9f8b0a3fe08cc3a00290cde6691dfb2581482f0
ananyabalagere <abalagere@gmail.com>
2024-03-15 18:58:25 -0700

    jonathan pls dont remove. implemented increment and decrement for wrist/claw values. finished tests

--- Armduino Test/Arm_Test1/Arm_Test1.ino
+++ Armduino Test/Arm_Test1/Arm_Test1.ino
@@ -59,18 +59,18 @@ void loop() {
   }
 }
 
-void wrist(int decrease, int increase) {
+int wrist(int increase, int decrease) {
   // increase == 1, decrease == 0: increase wrist value
   // increase == 0, decrease == 1: decrease wrist value
   // increase == 1, decrease == 1: do nothing
   // increase == 0, decrease == 0: do nothing
-  return (increase && !decrease) ? wristValue++ : ((decrease && !increase) ? wristValue-- : wristValue);
+  return (increase && !(decrease)) ? ++wristValue : ((decrease && !(increase)) ? --wristValue : wristValue);
 }
 
-void claw(int close, int open){
+int claw(int close, int open){
   // open == 1, close == 0: increase claw value
   // open == 0, close == 1: decrease claw value
   // open == 1, close == 1: do nothing
   // open == 0, close == 0: do nothing
-  return (open && !close) ? clawValue++ : ((close && !open) ? clawValue-- : clawValue);
+  return (open && !(close)) ? ++clawValue : ((close && !(open)) ? --clawValue : wristValue);
 }
